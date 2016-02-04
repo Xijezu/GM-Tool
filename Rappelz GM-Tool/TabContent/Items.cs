4b72717ed6bd71831c2bd10dd1a900b745aaaddf
Marcel Exner <marcel@xijezu.com>
2016-02-04 18:39:26 +0100

    Changed command parameters as well as standard value

--- Rappelz GM-Tool/TabContent/Items.cs
+++ Rappelz GM-Tool/TabContent/Items.cs
@@ -4,12 +4,19 @@ namespace GM_Tool_V5 {
     partial class frmGlobalUI {
         #region Items
         private void AddItem(object sender, EventArgs e) {
-            SFM.CopyToClipboard("/run insert_item({0}, {1}, {2}, {3}, 2, \"{4}\")",
-                                SFM.GetSelection(dgvItems),
-                                tbItemValue.Text,
-                                tbItemEnchant.Text,
-                                tbItemLevel.Text,
-                                GetSelectedCharacter());
+            if ( SFM.IsInt( tbItemEnchant.Text ) && SFM.IsInt( tbItemEnchant.Text )
+                && !SFM.IsNullOrEmpty( tbItemEnchant.Text ) && !SFM.IsNullOrEmpty( tbItemLevel.Text ) ) {
+                SFM.CopyToClipboard( "/run insert_item({0}, {1}, {2}, {3}, 2)",
+                                    SFM.GetSelection( dgvItems ),
+                                    ( SFM.IsInt( tbItemValue.Text ) ? tbItemValue.Text : "1" ),
+                                    tbItemEnchant.Text,
+                                    tbItemLevel.Text );
+            }
+            else {
+                SFM.CopyToClipboard( "/run insert_item({0}, {1})",
+                                 SFM.GetSelection( dgvItems ),
+                                 ( SFM.IsInt( tbItemValue.Text ) ? tbItemValue.Text : "1" ) );
+            }
         }
         #endregion
     }
