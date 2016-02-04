094a5c6e6b903e47e068c91ef4f90942035418ad
Marcel Exner <marcel@xijezu.com>
2016-02-04 18:38:12 +0100

    Set default values instead copying the string

--- Rappelz GM-Tool/TabContent/Buffs.cs
+++ Rappelz GM-Tool/TabContent/Buffs.cs
@@ -4,24 +4,33 @@ namespace GM_Tool_V5 {
     partial class frmGlobalUI {
         #region Buffs
         private void AddBuff(object sender, EventArgs e) {
-            // /run add_state(time, level, time, name)
-            SFM.CopyToClipboard("/run add_state({0}, {1}, {2}, \"{3}\")", SFM.GetSelection(dgvBuffs), tbBuffsLevel.Text, SFM.GetTimeValue(lboxBuffTime, tbBuffsTime), GetSelectedCharacter());
+            var time = SFM.GetTimeValue( lboxBuffTime, tbBuffsTime );
+            SFM.CopyToClipboard( "/run add_state({0}, {1}, {2}, \"{3}\")",
+                                SFM.GetSelection( dgvBuffs ),
+                                ( SFM.IsInt( tbBuffsLevel.Text ) ? tbBuffsLevel.Text : "1" ),
+                                ( time > 0 ? time : 360000 ),
+                                GetSelectedCharacter() );
         }
 
         private void AddCreatureBuff(object sender, EventArgs e) {
-            SFM.CopyToClipboard("/run add_cstate({0}, {1}, {2}, \"{3}\")", SFM.GetSelection(dgvBuffs), tbBuffsLevel.Text, SFM.GetTimeValue(lboxBuffTime, tbBuffsTime), GetSelectedCharacter());
+            var time = SFM.GetTimeValue( lboxBuffTime, tbBuffsTime );
+            SFM.CopyToClipboard( "/run add_cstate({0}, {1}, {2}, \"{3}\")", 
+                                SFM.GetSelection( dgvBuffs ),
+                                ( SFM.IsInt( tbBuffsLevel.Text ) ? tbBuffsLevel.Text : "1" ),
+                                ( time > 0 ? time : 360000 ),
+                                GetSelectedCharacter() );
         }
 
         private void AddEventBuff(object sender, EventArgs e) {
-            SFM.CopyToClipboard("/run add_event_state({0}, {1})", SFM.GetSelection(dgvBuffs), tbBuffsLevel.Text);
+            SFM.CopyToClipboard( "/run add_event_state({0}, {1})", SFM.GetSelection( dgvBuffs ), tbBuffsLevel.Text );
         }
 
         private void RemoveBuff(object sender, EventArgs e) {
-            SFM.CopyToClipboard("/run remove_state({0}, get_state_level({0}), \"{1}\")", SFM.GetSelection(dgvBuffs), GetSelectedCharacter());
+            SFM.CopyToClipboard( "/run remove_state({0}, get_state_level({0}), \"{1}\")", SFM.GetSelection( dgvBuffs ), GetSelectedCharacter() );
         }
 
         private void RemoveEventBuff(object sender, EventArgs e) {
-            SFM.CopyToClipboard("/run remove_event_state({0})", SFM.GetSelection(dgvBuffs));
+            SFM.CopyToClipboard( "/run remove_event_state({0})", SFM.GetSelection( dgvBuffs ) );
         }
 
         #endregion
--- Rappelz GM-Tool/TabContent/Monster.cs
+++ Rappelz GM-Tool/TabContent/Monster.cs
@@ -4,18 +4,20 @@ namespace GM_Tool_V5 {
     partial class frmGlobalUI {
         #region Monster
         private void SpawnMonster(object sender, EventArgs e) {
-            if (!cbCustomCoordinates.Checked) {
-                SFM.CopyToClipboard("//regenerate {0} {1}", SFM.GetSelection(dgvMonster), tbMonsterAmount.Text);
-            } else {
-                SFM.CopyToClipboard("/run add_npc({0}, {1}, {3}, {2})", tbMonsterX.Text, tbMonsterY.Text, tbMonsterAmount.Text, SFM.GetSelection(dgvMonster));
+            if ( !cbCustomCoordinates.Checked ) {
+                SFM.CopyToClipboard( "//regenerate {0} {1}", SFM.GetSelection( dgvMonster ), ( SFM.IsInt( tbMonsterAmount.Text ) ? tbMonsterAmount.Text : "1" ) );
+            }
+            else {
+                SFM.CopyToClipboard( "/run add_npc({0}, {1}, {3}, {2})", tbMonsterX.Text, tbMonsterY.Text, tbMonsterAmount.Text, SFM.GetSelection( dgvMonster ) );
             }
         }
 
         private void CustomCoordinatesCheckChanged(object sender, EventArgs e) {
-            if (cbCustomCoordinates.Checked) {
+            if ( cbCustomCoordinates.Checked ) {
                 tbMonsterX.Enabled = true;
                 tbMonsterY.Enabled = true;
-            } else {
+            }
+            else {
                 tbMonsterX.Enabled = false;
                 tbMonsterY.Enabled = false;
             }
